ea341174773af8d6a5f97a44288445147e732074
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-11-13 11:23:56 -0300

    arrumei o form :D

--- front/src/componentes/Form/Form.jsx
+++ front/src/componentes/Form/Form.jsx
@@ -12,17 +12,17 @@ function Form({tipo}){
                     </div>
                     <div className='inputForm' aria-label='Campo de email'>
                         <label>Email:</label>
-                        <input type='email' placeholder='Digite seu email' aria-label='Campo de email'></input>
+                        <input type='email' placeholder='Digite seu email' aria-label='Campo de email' required></input>
                     </div>
                     <div className='inputForm' aria-label='Campo de senha'>
                         <label>Senha:</label>
-                        <input type='password' placeholder='Digite sua senha' aria-label='Campo de senha'></input>
+                        <input type='password' placeholder='Digite sua senha' aria-label='Campo de senha' required></input>
                     </div>
                     <button type='submit' aria-label='Botão de entrar na conta'>Entrar</button>
-                    <p>Não tem uma conta? <a href='https://m.media-amazon.com/images/M/MV5BZDE2ZjIxYzUtZTJjYS00OWQ0LTk2NGEtMDliYmI3MzMwYjhkXkEyXkFqcGdeQWFsZWxvZw@@._V1_.jpg'>Cadastre-se</a></p>
+                    <p id='linkCadastro'>Não tem uma conta? <a href='https://m.media-amazon.com/images/M/MV5BZDE2ZjIxYzUtZTJjYS00OWQ0LTk2NGEtMDliYmI3MzMwYjhkXkEyXkFqcGdeQWFsZWxvZw@@._V1_.jpg'>Cadastre-se</a></p>
                 </form>
             </section>
-        ) : (
+        ) ?  (
             <section className='form' aria-label='formulario'>
                 <form className='formAzul'>
                     <div className='tituloFormulario'>
